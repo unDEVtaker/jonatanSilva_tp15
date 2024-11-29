48419a1c9820466421677f55d5ca2fdff0a8c68b
Jona Silva <jonatanemanuelsilva6@gmail.com>
2024-11-29 10:14:32 -0300

    add microdesafio 2

--- app.js
+++ app.js
@@ -3,13 +3,24 @@
 const peliculas = require('./peliculas');
 
 
+//funcion mostrarPeliculas, para mostrar el detalle
+const mostrarPeliculas = function() {
 //foreach para iterar los objetos del array peliculas y lo muestro con un console.log
-peliculas.forEach(pelicula => {
-    console.log(`ID: ${pelicula.id}`);
-    console.log(`Rating: ${pelicula.rating}`);
-    console.log(`Awards: ${pelicula.awards}`);
-    console.log(`Length: ${pelicula.length} min`);
-    console.log(`Price: $${pelicula.price}`);
-    console.log(`Genre: ${pelicula.genre}`);
-    console.log('-------------------------');
-});
+    peliculas.forEach(pelicula => {
+        console.log(`ID: ${pelicula.id}`);
+        console.log(`Rating: ${pelicula.rating}`);
+        console.log(`Awards: ${pelicula.awards}`);
+        console.log(`Length: ${pelicula.length} min`);
+        console.log(`Price: $${pelicula.price}`);
+        console.log(`Genre: ${pelicula.genre}`);
+        console.log('-------------------------');
+    });
+};
+
+mostrarPeliculas();
+
+//Micro desafío 2 (opcional):
+const fs = require('fs');
+const filePath = './mensaje.txt';
+const mensaje = fs.readFileSync(filePath, 'utf-8');
+console.log(mensaje);
